e7d123dbc918a3513d38a0ad51724a5bcd8fabac
Riz0tto <104946042+Riz0tto@users.noreply.github.com>
2024-04-16 14:38:55 +1200

    Tweaked faces

---
+++ randomised-collection-Riz0tto-main/example_arrangement.js
@@ -0,0 +1,73 @@
+/*
+ * This program draws your arrangement of faces on the canvas.
+ */
+
+const canvasWidth = 960;
+const canvasHeight = 500;
+let curRandomSeed = 0;
+
+let lastSwapTime = 0;
+const millisPerSwap = 3000;
+
+function setup () {
+  // create the drawing canvas, save the canvas element
+  let main_canvas = createCanvas(canvasWidth, canvasHeight);
+  main_canvas.parent('canvasContainer');
+
+  curRandomSeed = int(random(0, 1000));
+
+  // rotation in degrees
+  angleMode(DEGREES);
+}
+
+function changeRandomSeed() {
+  curRandomSeed = curRandomSeed + 1;
+  lastSwapTime = millis();
+}
+
+// global variables for colors
+const bg_color1 = [200, 200, 200]
+
+function mouseClicked() {
+  changeRandomSeed();
+}
+
+function draw () {
+  if(millis() > lastSwapTime + millisPerSwap) {
+    changeRandomSeed();
+  }
+
+  // reset the random number generator each time draw is called
+  randomSeed(curRandomSeed);
+
+  // clear screen
+  background(bg_color1);
+  noStroke();
+
+  // draw a 7x4 grid of faces
+  let w = canvasWidth / 7;
+  let h = canvasHeight / 4;
+  for(let i=0; i<4; i++) {
+    for(let j=0; j<7; j++) {
+      let y = h/2 + h*i;
+      let x = w/2 + w*j;
+
+      push();
+      translate(x, y);
+      scale(w/25, h/25);
+
+      blobFace(random(360), random(2, 4), random(2, 4), random(3, 5), random(300), 0, 0);
+      pop();
+
+    }
+  }
+}
+
+function keyTyped() {
+  if (key == '!') {
+    saveBlocksImages();
+  }
+  else if (key == '@') {
+    saveBlocksImages(true);
+  }
+}
