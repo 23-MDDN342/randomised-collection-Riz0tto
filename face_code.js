9fb9f302454a5207f41c0414c059cee23d08b6d5
Riz0tto <104946042+Riz0tto@users.noreply.github.com>
2024-04-27 02:38:16 +1200

    Added head tilt variable

--- face_code.js
+++ face_code.js
@@ -46,6 +46,12 @@ function blobbyFace(eye1_x, eye1_y, eye1_r, eye2_x, eye2_y, eye2_r, hue, eye_sel
     r: dist(eye1_x, eye1_y, eye2_x, eye2_y)/2
   };
 
+  // finding the rotation of the head so facial features can be rotated with it, head rotation is based on the position of the eyes - it is not created and then rotated
+  var eye1_v = createVector(eye1_x, eye1_y);
+  var eye2_v = createVector(eye2_x, eye2_y);
+  var head_tilt = atan2(eye2_y - eye1_y, eye2_x - eye1_x); // I used ChatGPT 3.5 to get this function, the prompt was: "I need to get get the rotation between two points in p5.js"
+  
+
   push();
   colorMode(HSB);
   ellipseMode(RADIUS);
@@ -63,8 +69,7 @@ function blobbyFace(eye1_x, eye1_y, eye1_r, eye2_x, eye2_y, eye2_r, hue, eye_sel
   ellipse(head.x, head.y, head.r);
   fill(hue, 60, 100);
   ellipse(eye1_x, eye1_y, eye1_r);
-  ellipse(eye2_x, eye2_y, eye2_r);
-  
+  ellipse(eye2_x, eye2_y, eye2_r);  
 
   // draw eyes
   fill(100);
@@ -76,6 +81,15 @@ function blobbyFace(eye1_x, eye1_y, eye1_r, eye2_x, eye2_y, eye2_r, hue, eye_sel
   fill(100);
   ellipse(eye1_x-(eye1_r/4), eye1_y-(eye1_r/4), eye1_r*0.2);
   ellipse(eye2_x-(eye2_r/4), eye2_y-(eye2_r/4), eye2_r*0.2);
+
+  // draw mouth
+
+  push();
+  translate(head.x, head.y);
+  rotate(head_tilt); 
+  strokeWeight(width/300);
+  line(-head.r/3, head.r/2, head.r/3, head.r/2);
+  pop();
 
   pop();
 }
